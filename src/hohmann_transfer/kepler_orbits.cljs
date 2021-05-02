1a97769e22a099f724d20c8ecd4eb0cb065a2f39
rival <ri.valentini@reply.de>
2021-05-02 13:07:35 +0200

    Restrict orbital-elements input configuration to visually legal ranges

--- src/hohmann_transfer/kepler_orbits.cljs
+++ src/hohmann_transfer/kepler_orbits.cljs
@@ -45,8 +45,8 @@
   (draw-dotted-kepler-orbit @state))
 
 (defmethod s/build-state :kepler-orbits []
-  {:center-of-gravity {:x (/ s/width 2)
-                       :y (/ s/height 2)
+  {:center-of-gravity {:x      (/ s/width 2)
+                       :y      (/ s/height 2)
                        :radius 10}
    :elliptical-orbit  {:t           0
                        :mass        3.674E23
@@ -54,7 +54,8 @@
                        :e           0.6
                        :i           0
                        :small-omega 0
-                       :big-omega   (/ Math/PI 2)}})
+                       :big-omega   (/ Math/PI 2)}
+   :view {:locked-controls []}})
 
 (defmethod s/render-sketch :kepler-orbits [_ state-setup]
   (q/defsketch kepler-orbits
@@ -68,30 +69,66 @@
 
 ;; UI Components
 
+(defn check-invariants [radius e a]
+  (let [semi-minor-axis (* a (Math/sqrt (- 1 (Math/pow e 2))))
+        invariant-a (< (+ 5 radius) (- a (* e a)))
+        invariant-b (> semi-minor-axis (+ 5 radius))]
+    (and invariant-a invariant-b)))
+
+(defmulti invariants-satisfied? (fn [param & _] param))
+
+(defmethod invariants-satisfied? :mass [_ app-state new-radius]
+  (let [a (get-in app-state [:elliptical-orbit :a])
+        e (get-in app-state [:elliptical-orbit :e])]
+    (check-invariants new-radius e a)))
+
+(defmethod invariants-satisfied? :e [_ app-state new-ecc]
+  (let [a (get-in app-state [:elliptical-orbit :a])
+        radius (get-in app-state [:center-of-gravity :radius])]
+    (check-invariants radius new-ecc a)))
+
+(defmethod invariants-satisfied? :a [_ app-state new-a]
+  (let [e (get-in app-state [:elliptical-orbit :e])
+        radius (get-in app-state [:center-of-gravity :radius])]
+    (check-invariants radius e new-a)))
+
+(defmethod invariants-satisfied? :big-omega [_] true)
+
 (defn slider [app-state param value scale]
-  (let [slider-scale {:min 0 :max 100}]
+  (let [slider-scale {:min 0 :max 100}
+        locked-controls (get-in @app-state [:view :locked-controls])]
     [:input {:type      "range" :value (s/transform-scale scale slider-scale value) :min 0 :max 100
+             :class     (when (some #(= param %) locked-controls) "red")
              :on-change (fn [e]
                           (let [new-value (js/parseInt (.. e -target -value))
-                                new-value-scaled (s/transform-scale slider-scale scale new-value)]
-                            (if (= param :mass)
-                              (let [new-radius (s/transform-scale slider-scale {:min 3 :max 30} new-value)]
-                                (swap! app-state assoc-in [:center-of-gravity :radius] new-radius)))
-                            (swap! app-state assoc-in [:elliptical-orbit param] new-value-scaled)))}]))
+                                new-value-scaled (s/transform-scale slider-scale scale new-value)
+                                new-radius (s/transform-scale slider-scale {:min 3 :max 30} new-value)
+                                invariants-ok (if (= param :mass)
+                                                (invariants-satisfied? param @app-state new-radius)
+                                                (invariants-satisfied? param @app-state new-value-scaled))]
+                            (swap! app-state #(if invariants-ok
+                                                (-> %
+                                                  ((fn [state]
+                                                     (if (= param :mass)
+                                                       (assoc-in state [:center-of-gravity :radius] new-radius)
+                                                       state)))
+                                                  (assoc-in [:elliptical-orbit param] new-value-scaled)
+                                                  (assoc-in [:view :locked-controls] (remove #{param} locked-controls)))
+                                                (assoc-in % [:view :locked-controls] (conj locked-controls param))))))}]))
 
 (defn controls [app-state]
-  (let [{:keys [e a mass big-omega]} (:elliptical-orbit @app-state)] ;TODO make generic and move to :kepler-orbit
+  (let [{:keys [e a mass big-omega]} (:elliptical-orbit @app-state)]
     [:div.db.dtc-ns.v-mid.ph2.pr0-ns.pl3-ns
-     [:div.lh-copy
+     [:div.w4.f6.lh-copy
       "Eccentricity"
       [slider app-state :e e {:min 0.01 :max 0.90}]]
-     [:div.lh-copy
+     [:div.w4.f6.lh-copy
       "Big Omega"
       [slider app-state :big-omega big-omega {:min 0 :max (* 2 Math/PI)}]]
-     [:div.lh-copy
+     [:div.w4.f6.lh-copy
       "Mass of central body"
       [slider app-state :mass mass {:min 1.00E22 :max 2.00E24}]]
-     [:div.lh-copy
+     [:div.w4.f6.lh-copy
       "Length of the semimajor axis"
       [slider app-state :a a {:min 37 :max 300}]]]))
 
