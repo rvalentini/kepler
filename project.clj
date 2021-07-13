b5f5f3ce33f4b53b2cc54e1211d5672124b04709
rival <ri.valentini@reply.de>
2021-07-13 17:16:06 +0200

    Fix project.clj and increase deps versions

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject kepler "0.1.0-SNAPSHOT"
-  :description "FIXME: write this!"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject kepler "0.1.0"
+  :description "Interactive website explaining the three Kepler laws of planetary motion using animations"
+  :url "https://github.com/rvalentini/kepler"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
 
   :min-lein-version "2.7.1"
 
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/clojurescript "1.10.773"]
-                 [reagent "0.10.0" ]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/clojurescript "1.10.844"]
+                 [reagent "1.0.0" ]
                  [quil "3.1.0"]]
 
   :source-paths ["src"]
@@ -17,7 +17,7 @@
             "fig:build" ["trampoline" "run" "-m" "figwheel.main" "-b" "dev" "-r"]
             "fig:min"   ["run" "-m" "figwheel.main" "-O" "advanced" "-bo" "dev"]}
 
-  :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.2.12"]]
+  :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.2.13"]]
                    :resource-paths ["target"]
                    ;; need to add the compiled assets to the :clean-targets
                    :clean-targets ^{:protect false} ["target"]}})
